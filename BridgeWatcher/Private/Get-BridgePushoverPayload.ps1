d187b06b9701f94c15edd8255d9747b98d2a7a89
Jay Cap <3140504+mrjcap@users.noreply.github.com>
2026-07-01 21:56:59 +0300

    refactor(Get-BridgePushoverPayload): translate comments, documentation, and logs to Greek

--- BridgeWatcher/Private/Get-BridgePushoverPayload.ps1
+++ BridgeWatcher/Private/Get-BridgePushoverPayload.ps1
@@ -1,5 +1,4 @@
-﻿function Get-BridgePushoverPayload {
-    [CmdletBinding()]
+﻿function Get-BridgePushoverPayload {
     <#
     .SYNOPSIS
     Δημιουργεί σώμα αιτήματος για αποστολή μέσω Pushover.
@@ -21,35 +20,36 @@
     [hashtable] - Το payload σε μορφή hashtable.
 
     .EXAMPLE
-    Get-BridgePushoverPayload -PoUserKey 'user123' -PoApiKey 'token123' -Message 'Bridge Closed!'
+    Get-BridgePushoverPayload -PoUserKey 'user123' -PoApiKey 'token123' -Message 'Η γέφυρα έκλεισε!'
 
     .NOTES
-    Υποστηρίζει επίσης optional fields όπως Device, Title, Url, Priority, Sound.
-    #>
-
-    [OutputType([hashtable])]
-    param (
-        [Parameter(Mandatory)][string]$PoUserKey,
-        [Parameter(Mandatory)][string]$PoApiKey,
-        [Parameter(Mandatory)][string]$Message,
-        [string]$Device,
-        [string]$Title,
-        [string]$Url,
-        [string]$UrlTitle,
-        [int]$Priority,
-        [string]$Sound
-    )
-
-    $data = @{
-        token   = $PoApiKey
-        user    = $PoUserKey
-        message = $Message
-    }
-    if ($PSBoundParameters.ContainsKey('Device') -and $Device) { $data.device = $Device }
-    if ($PSBoundParameters.ContainsKey('Title') -and $Title) { $data.title = $Title }
-    if ($PSBoundParameters.ContainsKey('Url') -and $Url) { $data.url = $Url }
-    if ($PSBoundParameters.ContainsKey('UrlTitle') -and $UrlTitle) { $data.url_title = $UrlTitle }
-    if ($PSBoundParameters.ContainsKey('Priority') -and $Priority) { $data.priority = $Priority }
-    if ($PSBoundParameters.ContainsKey('Sound') -and $Sound) { $data.sound = $Sound }
-    return $data
-}
+    Υποστηρίζει επίσης προαιρετικά πεδία όπως Device, Title, Url, Priority, Sound.
+    #>
+    [CmdletBinding()]
+    [OutputType([hashtable])]
+    param (
+        [Parameter(Mandatory)][string]$PoUserKey,
+        [Parameter(Mandatory)][string]$PoApiKey,
+        [Parameter(Mandatory)][string]$Message,
+        [string]$Device,
+        [string]$Title,
+        [string]$Url,
+        [string]$UrlTitle,
+        [int]$Priority,
+        [string]$Sound
+    )
+
+    $data = @{
+        token   = $PoApiKey
+        user    = $PoUserKey
+        message = $Message
+    }
+    if ($PSBoundParameters.ContainsKey('Device') -and $Device) { $data.device = $Device }
+    if ($PSBoundParameters.ContainsKey('Title') -and $Title) { $data.title = $Title }
+    if ($PSBoundParameters.ContainsKey('Url') -and $Url) { $data.url = $Url }
+    if ($PSBoundParameters.ContainsKey('UrlTitle') -and $UrlTitle) { $data.url_title = $UrlTitle }
+    if ($PSBoundParameters.ContainsKey('Priority') -and $Priority) { $data.priority = $Priority }
+    if ($PSBoundParameters.ContainsKey('Sound') -and $Sound) { $data.sound = $Sound }
+    return $data
+}
+
