4c0327a8e5feb69542c834b78c44f643486bdbcc
Jay Cap <3140504+mrjcap@users.noreply.github.com>
2026-07-03 06:11:15 +0300

    style: normalize line endings and restore utf-8 bom

--- BridgeWatcher/Private/Get-BridgeImage.ps1
+++ BridgeWatcher/Private/Get-BridgeImage.ps1
@@ -1,4 +1,4 @@
-﻿function Get-BridgeImage {
+﻿function Get-BridgeImage {
     <#
     .SYNOPSIS
     Εξάγει όλους τους συνδέσμους εικόνων από HTML περιεχόμενο.
@@ -22,45 +22,45 @@
     .NOTES
     Η ανάλυση βασίζεται σε κανονικές εκφράσεις (regex) για την εύρεση εικόνων.
     Φιλτράρει μόνο τις εικόνες που περιέχουν 'image-bridge' στο URL.
-    #>
-    [CmdletBinding()]
-    [OutputType([System.Collections.ArrayList])]
-    param (
-        [Parameter(Mandatory)][string]$HtmlContent,
-        [Parameter(Mandatory)][ValidateSet('poseidonia', 'isthmia')] [string]$Location
-    )
-    # Αφαίρεση τόνων από το μοτίβο (pattern) για αντιστοίχιση τόσο τονισμένου όσο και ατόνιστου ελληνικού κειμένου
-    $bridgePattern = if ($Location -eq 'poseidonia') { 'Ποσειδων[ιίΙΊ]α' } else { 'Ισθμ[ιίΙΊ]α' }
-
-    # Διαχωρισμός με χρήση ανθεκτικού regex για χειρισμό μη ευαισθησίας πεζών-κεφαλαίων, μονών/διπλών εισαγωγικών και μεταβλητών κενών/κλάσεων
-    $blocks = [regex]::Split($HtmlContent, '(?i)<div[^>]+class=["''][^"'']*panel\s+panel-primary[^"'']*["''][^>]*>')
-
-    # Αντιστοίχιση μπλοκ (block) χωρίς ευαισθησία πεζών-κεφαλαίων με βάση το μοτίβο της γέφυρας
-    $block = $blocks | Where-Object { $_ -match "(?i)$bridgePattern" }
-    if (-not $block) {
-        $writeBridgeLogSplat = @{
-            Stage   = 'Ανάλυση'
-            Message = "❌ Δεν βρέθηκε block για $Location"
-        }
-        Write-BridgeLog @writeBridgeLogSplat
-
-        $errorRecord = [System.Management.Automation.ErrorRecord]::new(
-            ([System.Exception]::new("Δεν βρέθηκε block για τη θέση $Location.")),
-            'BridgeImageBlockNotFound',
-            [System.Management.Automation.ErrorCategory]::ObjectNotFound,
-            $Location
-        )
-        $PSCmdlet.ThrowTerminatingError($errorRecord)
-    }
-    # Αντιστοίχιση ετικετών img χωρίς ευαισθησία πεζών-κεφαλαίων, με μονά/διπλά εισαγωγικά και αυθαίρετη σειρά του src
-    $bmatches = [regex]::Matches($block, '(?i)<img[^>]+src=["'']([^"'']+)["'']')
-    $imageList = [System.Collections.ArrayList]::new()
-    foreach ($m in $bmatches) {
-        $src = $m.Groups[1].Value
-        if ($src -notmatch '\.png$' -and $src -match 'image-bridge') {
-            $null = $imageList.Add([pscustomobject]@{ src = $src })
-        }
-    }
-    return [System.Collections.ArrayList]::new([object[]]@($imageList))
-}
-
+    #>
+    [CmdletBinding()]
+    [OutputType([System.Collections.ArrayList])]
+    param (
+        [Parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$HtmlContent,
+        [Parameter(Mandatory)][ValidateNotNullOrEmpty()][ValidateSet('poseidonia', 'isthmia')] [string]$Location
+    )
+    # Αφαίρεση τόνων από το μοτίβο (pattern) για αντιστοίχιση τόσο τονισμένου όσο και ατόνιστου ελληνικού κειμένου
+    $bridgePattern = if ($Location -eq 'poseidonia') { 'Ποσειδων[ιίΙΊ]α' } else { 'Ισθμ[ιίΙΊ]α' }
+
+    # Διαχωρισμός με χρήση ανθεκτικού regex για χειρισμό μη ευαισθησίας πεζών-κεφαλαίων, μονών/διπλών εισαγωγικών και μεταβλητών κενών/κλάσεων
+    $blocks = [regex]::Split($HtmlContent, '(?i)<div[^>]+class=["''][^"'']*panel\s+panel-primary[^"'']*["''][^>]*>')
+
+    # Αντιστοίχιση μπλοκ (block) χωρίς ευαισθησία πεζών-κεφαλαίων με βάση το μοτίβο της γέφυρας
+    $block = $blocks | Where-Object { $_ -match "(?i)$bridgePattern" }
+    if (-not $block) {
+        $writeBridgeLogSplat = @{
+            Stage   = 'Ανάλυση'
+            Message = "❌ Δεν βρέθηκε block για $Location"
+        }
+        Write-BridgeLog @writeBridgeLogSplat
+
+        $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+            ([System.Exception]::new("Δεν βρέθηκε block για τη θέση $Location.")),
+            'BridgeImageBlockNotFound',
+            [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+            $Location
+        )
+        $PSCmdlet.ThrowTerminatingError($errorRecord)
+    }
+    # Αντιστοίχιση ετικετών img χωρίς ευαισθησία πεζών-κεφαλαίων, με μονά/διπλά εισαγωγικά και αυθαίρετη σειρά του src
+    $bmatches = [regex]::Matches($block, '(?i)<img[^>]+src=["'']([^"'']+)["'']')
+    $imageList = [System.Collections.ArrayList]::new()
+    foreach ($m in $bmatches) {
+        $src = $m.Groups[1].Value
+        if ($src -notmatch '\.png$' -and $src -match 'image-bridge') {
+            $null = $imageList.Add([pscustomobject]@{ src = $src })
+        }
+    }
+    return [System.Collections.ArrayList]::new([object[]]@($imageList))
+}
+
